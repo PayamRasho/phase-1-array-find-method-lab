9f1e6fac0aef6b5dd4de25a2079338691c79f3b9
PayamRasho <payamrfattah01@gmail.com>
2024-02-23 18:24:53 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+  const winningRecord = record.find((game) => game.result === "W");
+
+  return winningRecord ? winningRecord.year : undefined;
+}
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+];
+
+console.log(superbowlWin(record)); // Output: 2017
